c7bb9785b29e54b84d7451be7ae831663f302aa8
Dmitry <di.mesler.2010@yandex.ru>
2022-12-15 01:14:05 +0300

    Feat: Show help message with flag -h

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python3
+import argparse
 
 
 def main():
-    print('test')
+    parser = argparse.ArgumentParser(
+        description='Compares two configuration files and shows a difference.'
+    )
+    parser.add_argument('first_file')
+    parser.add_argument('second_file')
+
+    args = parser.parse_args()
+    print(args)
 
 
 if __name__ == '__main__':
