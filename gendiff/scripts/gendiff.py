78ae2a54a245c00cea78b21b312d8241000f0798
Dmitry <di.mesler.2010@yandex.ru>
2023-03-20 16:14:28 +0300

    Feat: Add optional argument --format

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -8,9 +8,10 @@ def main():
     )
     parser.add_argument('first_file')
     parser.add_argument('second_file')
+    parser.add_argument('-f', '--format', help='set format of output')
 
     args = parser.parse_args()
-    print(args)
+    print(args.accumulate(args.integers))
 
 
 if __name__ == '__main__':
