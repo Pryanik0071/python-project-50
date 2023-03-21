80d46e535fb7e7518e3bdc09239d4f547c65fb96
Dmitry <di.mesler.2010@yandex.ru>
2023-03-21 17:36:29 +0300

    Refactoring: rename is_different to check_values

--- gendiff/__init__.py
+++ gendiff/__init__.py
@@ -12,11 +12,11 @@ def generate_diff(file_path1, file_path2):
     keys = sorted(set(list(json1.keys()) + list(json2.keys())))
     dict_ = {}
     for key in keys:
-        dict_.update(is_different(key, json1.get(key), json2.get(key)))
+        dict_.update(check_values(key, json1.get(key), json2.get(key)))
     return json.dumps(dict_, indent=4).replace('\"', '')
 
 
-def is_different(key, value1, value2):
+def check_values(key, value1, value2):
     if value1 and value2:
         if value1 != value2:
             return {
