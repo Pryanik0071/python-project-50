31e415fbcd778f910f17cd7c04536b11d3230bf8
Dmitry <di.mesler.2010@yandex.ru>
2023-04-13 15:04:29 +0300

    Tests: Add file reader tests

---
+++ tests/test_reader.py
@@ -0,0 +1,17 @@
+import pytest
+
+from gendiff.reader import read_file
+
+
+PATH = 'tests/fixtures/'
+
+
+@pytest.mark.parametrize("file_path, expected", [
+    (PATH + 'file1.json', True),
+    (PATH + 'file1.yaml', True),
+    (PATH + 'file1.yml', True),
+    (PATH + 'file1.txt', False)
+
+])
+def test_reader(file_path, expected):
+    assert bool(read_file(file_path)) == expected
