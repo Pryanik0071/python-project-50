8d5b928ece239768d245fe549ddd0f538732cfa7
Dmitry <di.mesler.2010@yandex.ru>
2023-04-15 20:10:36 +0300

    Refactoring: Reader + Tests for reader, add fixtures

--- gendiff/reader.py
+++ gendiff/reader.py
@@ -4,10 +4,26 @@ import pathlib
 import yaml
 
 
+def read_json(file_path):
+    file = dict()
+    try:
+        file = json.load(open(file_path))
+    except json.decoder.JSONDecodeError:
+        print(f'File {file_path} is empty!')
+    return file
+
+
+def read_yaml(file_path):
+    with open(file_path) as f:
+        file = yaml.load(f, Loader=yaml.loader.SafeLoader)
+    if file is None:
+        return dict()
+    return file
+
+
 def read_file(file_path):
     if pathlib.Path(file_path).suffix == '.json':
-        return json.load(open(file_path))
+        return read_json(file_path)
     elif pathlib.Path(file_path).suffix in ('.yaml', '.yml'):
-        with open(file_path) as f:
-            return yaml.load(f, Loader=yaml.loader.SafeLoader)
+        return read_yaml(file_path)
     print('File extension does\'t support, use .json or .yaml/.yml')
--- tests/test_reader.py
+++ tests/test_reader.py
@@ -1,5 +1,6 @@
 import pytest
 
+from .fixtures import objects as obj
 from gendiff.reader import read_file
 
 
@@ -7,10 +8,13 @@ PATH = 'tests/fixtures/'
 
 
 @pytest.mark.parametrize("file_path, expected", [
-    (PATH + 'file1.json', True),
-    (PATH + 'file1.yaml', True),
-    (PATH + 'file1.yml', True),
-    (PATH + 'file1.txt', False)
+    (PATH + 'file1.json', obj.file1),
+    (PATH + 'file1.yaml', obj.file1),
+    (PATH + 'file1.yml', obj.file1),
+    (PATH + 'file1.txt', None),
+    (PATH + 'file0.json', {}),
+    (PATH + 'file_empty.json', {}),
+    (PATH + 'file_empty.yml', {}),
 ])
 def test_reader(file_path, expected):
-    assert bool(read_file(file_path)) == expected
+    assert read_file(file_path) == expected
