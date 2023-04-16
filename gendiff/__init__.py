60451d06dda65c708565359605f833cf8b1a162e
Dmitry <di.mesler.2010@yandex.ru>
2023-04-16 19:59:48 +0300

    BugFix: generate_diff function

--- gendiff/__init__.py
+++ gendiff/__init__.py
@@ -12,6 +12,8 @@ def generate_diff(file_path1, file_path2, formatter):
     file1 = read_file(file_path1)
     file2 = read_file(file_path2)
     if file1 is None or file2 is None:
-        return None
+        return
     keys = get_first_keys(file1, file2)
-    return call_formatter(formatter)(build_diff_tree(keys, file1, file2))
+    if (format_func := call_formatter(formatter)) is None:
+        return
+    return format_func(build_diff_tree(keys, file1, file2))
