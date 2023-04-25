05e004ec6952d5077fc19c74a5b243bc25657016
Dmitry <di.mesler.2010@yandex.ru>
2023-04-25 21:34:47 +0300

    Refactoring: Delete debug objects for plain formatter

--- gendiff/formatters/plain.py
+++ gendiff/formatters/plain.py
@@ -1,31 +1,3 @@
-diff34 = [
-    {'key': 'common', 'status': 'NESTED', 'value': [
-        {'key': 'follow', 'status': 'ADD', 'value': False},
-        {'key': 'setting1', 'status': 'STAY', 'value': 'Value 1'},
-        {'key': 'setting2', 'status': 'DEL', 'value': 200},
-        {'key': 'setting3', 'status': 'CHANGE', 'value1_old': True, 'value2_new': None},
-        {'key': 'setting4', 'status': 'ADD', 'value': 'blah blah'},
-        {'key': 'setting5', 'status': 'ADD', 'value': {'key5': 'value5'}},
-        {'key': 'setting6', 'status': 'NESTED', 'value': [
-            {'key': 'doge', 'status': 'NESTED', 'value': [
-                {'key': 'wow', 'status': 'CHANGE', 'value1_old': '', 'value2_new': 'so much'}
-            ]},
-            {'key': 'key', 'status': 'STAY', 'value': 'value'},
-            {'key': 'ops', 'status': 'ADD', 'value': 'vops'}
-        ]}
-    ]},
-    {'key': 'group1', 'status': 'NESTED', 'value': [
-        {'key': 'baz', 'status': 'CHANGE', 'value1_old': 'bas', 'value2_new': 'bars'},
-        {'key': 'foo', 'status': 'STAY', 'value': 'bar'},
-        {'key': 'nest', 'status': 'CHANGE', 'value1_old': {'key': 'value'}, 'value2_new': 'str'}
-    ]},
-    {'key': 'group2', 'status': 'DEL', 'value': {'abc': 12345, 'deep': {'id': 45}}},
-    {'key': 'group3', 'status': 'ADD', 'value': {'deep': {'id': {'number': 45}},
-                                                 'fee': 100500}}
-]
-
-
-
 def get_head(key):
     return f'Property \'{key}\' was '
 
@@ -67,7 +39,3 @@ def get_plain_diff(node):
     for _ in node:
         list_.append(build_plain_tree(_, ''))
     return '\n'.join(list_).replace('\n\n', '\n')
-
-
-if __name__ == '__main__':
-    print(get_plain_diff(diff34))
