8d8d32b414af1222a0ede09b5936de3dd190b90f
Dmitry <di.mesler.2010@yandex.ru>
2023-04-25 20:36:51 +0300

    BugFix: plain formatter - bool values

--- gendiff/formatters/plain.py
+++ gendiff/formatters/plain.py
@@ -1,3 +1,31 @@
+diff34 = [
+    {'key': 'common', 'status': 'NESTED', 'value': [
+        {'key': 'follow', 'status': 'ADD', 'value': False},
+        {'key': 'setting1', 'status': 'STAY', 'value': 'Value 1'},
+        {'key': 'setting2', 'status': 'DEL', 'value': 200},
+        {'key': 'setting3', 'status': 'CHANGE', 'value1_old': True, 'value2_new': None},
+        {'key': 'setting4', 'status': 'ADD', 'value': 'blah blah'},
+        {'key': 'setting5', 'status': 'ADD', 'value': {'key5': 'value5'}},
+        {'key': 'setting6', 'status': 'NESTED', 'value': [
+            {'key': 'doge', 'status': 'NESTED', 'value': [
+                {'key': 'wow', 'status': 'CHANGE', 'value1_old': '', 'value2_new': 'so much'}
+            ]},
+            {'key': 'key', 'status': 'STAY', 'value': 'value'},
+            {'key': 'ops', 'status': 'ADD', 'value': 'vops'}
+        ]}
+    ]},
+    {'key': 'group1', 'status': 'NESTED', 'value': [
+        {'key': 'baz', 'status': 'CHANGE', 'value1_old': 'bas', 'value2_new': 'bars'},
+        {'key': 'foo', 'status': 'STAY', 'value': 'bar'},
+        {'key': 'nest', 'status': 'CHANGE', 'value1_old': {'key': 'value'}, 'value2_new': 'str'}
+    ]},
+    {'key': 'group2', 'status': 'DEL', 'value': {'abc': 12345, 'deep': {'id': 45}}},
+    {'key': 'group3', 'status': 'ADD', 'value': {'deep': {'id': {'number': 45}},
+                                                 'fee': 100500}}
+]
+
+
+
 def get_head(key):
     return f'Property \'{key}\' was '
 
@@ -5,14 +33,14 @@ def get_head(key):
 def get_value(value):
     if isinstance(value, dict):
         return '[complex value]'
-    if isinstance(value, int):
-        return value
     if isinstance(value, bool) or value is None:
         return {
             False: 'false',
             True: 'true',
             None: 'null',
         }.get(value)
+    if isinstance(value, int):
+        return value
     return f'\'{value}\''
 
 
@@ -39,3 +67,7 @@ def get_plain_diff(node):
     for _ in node:
         list_.append(build_plain_tree(_, ''))
     return '\n'.join(list_).replace('\n\n', '\n')
+
+
+if __name__ == '__main__':
+    print(get_plain_diff(diff34))
