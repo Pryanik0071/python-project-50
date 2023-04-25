d9143d4b5f968ea03630fe123f6196bc8a6d5c51
Dmitry <di.mesler.2010@yandex.ru>
2023-04-25 22:44:22 +0300

    Refactoring: Lint errors fixtures/objects

--- tests/fixtures/objects.py
+++ tests/fixtures/objects.py
@@ -19,7 +19,8 @@ diff12 = [
           {'key': 'follow', 'status': 'DEL', 'value': False},
           {'key': 'host', 'status': 'STAY', 'value': 'hexlet.io'},
           {'key': 'proxy', 'status': 'DEL', 'value': '123.234.53.22'},
-          {'key': 'timeout', 'status': 'CHANGE', 'value1_old': 50, 'value2_new': 20},
+          {'key': 'timeout', 'status': 'CHANGE',
+           'value1_old': 50, 'value2_new': 20},
           {'key': 'verbose', 'status': 'ADD', 'value': True}
 ]
 
@@ -28,12 +29,14 @@ diff34 = [
         {'key': 'follow', 'status': 'ADD', 'value': False},
         {'key': 'setting1', 'status': 'STAY', 'value': 'Value 1'},
         {'key': 'setting2', 'status': 'DEL', 'value': 200},
-        {'key': 'setting3', 'status': 'CHANGE', 'value1_old': True, 'value2_new': None},
+        {'key': 'setting3', 'status': 'CHANGE',
+         'value1_old': True, 'value2_new': None},
         {'key': 'setting4', 'status': 'ADD', 'value': 'blah blah'},
         {'key': 'setting5', 'status': 'ADD', 'value': {'key5': 'value5'}},
         {'key': 'setting6', 'status': 'NESTED', 'value': [
             {'key': 'doge', 'status': 'NESTED', 'value': [
-                {'key': 'wow', 'status': 'CHANGE', 'value1_old': '', 'value2_new': 'so much'}
+                {'key': 'wow', 'status': 'CHANGE',
+                 'value1_old': '', 'value2_new': 'so much'}
             ]},
             {'key': 'key', 'status': 'STAY', 'value': 'value'},
             {'key': 'ops', 'status': 'ADD', 'value': 'vops'}
@@ -42,9 +45,12 @@ diff34 = [
     {'key': 'group1', 'status': 'NESTED', 'value': [
         {'key': 'baz', 'status': 'CHANGE', 'value1_old': 'bas', 'value2_new': 'bars'},
         {'key': 'foo', 'status': 'STAY', 'value': 'bar'},
-        {'key': 'nest', 'status': 'CHANGE', 'value1_old': {'key': 'value'}, 'value2_new': 'str'}
+        {'key': 'nest', 'status': 'CHANGE',
+         'value1_old': {'key': 'value'}, 'value2_new': 'str'}
     ]},
-    {'key': 'group2', 'status': 'DEL', 'value': {'abc': 12345, 'deep': {'id': 45}}},
-    {'key': 'group3', 'status': 'ADD', 'value': {'deep': {'id': {'number': 45}},
-                                                 'fee': 100500}}
+    {'key': 'group2', 'status': 'DEL',
+     'value': {'abc': 12345, 'deep': {'id': 45}}},
+    {'key': 'group3', 'status': 'ADD',
+     'value': {'deep': {'id': {'number': 45}},
+               'fee': 100500}}
 ]
