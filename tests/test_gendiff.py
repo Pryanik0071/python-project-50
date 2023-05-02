3c67de1dc0e6a0b519bb4151b11cf46834e31221
Dmitry <di.mesler.2010@yandex.ru>
2023-05-02 17:58:11 +0300

    Refactoring: tests

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -7,26 +7,17 @@ PATH = 'tests/fixtures/'
 
 
 @pytest.mark.parametrize('file_path1, file_path2, formatter, expected', [
-    (PATH + 'file1.json', PATH + 'file2.json', 'stylish', PATH + 'stylish12.txt'),
-    (PATH + 'file1.json', PATH + 'file2.yaml', 'stylish', PATH + 'stylish12.txt'),
-    (PATH + 'file1.yaml', PATH + 'file2.yml', 'stylish', PATH + 'stylish12.txt'),
-    (PATH + 'file1.yaml', PATH + 'file2.json', 'stylish', PATH + 'stylish12.txt'),
-    (PATH + 'file3.json', PATH + 'file4.json', 'stylish', PATH + 'stylish34.txt'),
-    (PATH + 'file0.json', PATH + 'file1.json', 'stylish', PATH + 'stylish01.txt'),
-    (PATH + 'file3.json', PATH + 'file4.json', 'plain', PATH + 'plain34.txt'),
+    (f'{PATH}file1.json', f'{PATH}file2.yaml', 'stylish', f'{PATH}stylish12.txt'),
+    (f'{PATH}file1.yaml', f'{PATH}file2.yml', 'stylish', f'{PATH}stylish12.txt'),
+    (f'{PATH}file3.json', f'{PATH}file4.json', 'stylish', f'{PATH}stylish34.txt'),
+    (f'{PATH}file0.json', f'{PATH}file1.json', 'stylish', f'{PATH}stylish01.txt'),
+    (f'{PATH}file3.json', f'{PATH}file4.json', 'plain', f'{PATH}plain34.txt'),
 ])
 def test_generate_diff(file_path1, file_path2, formatter, expected):
-    assert generate_diff(file_path1, file_path2, formatter) == \
-           open(expected).read()
-
-
-@pytest.mark.parametrize('file_path1, file_path2, formatter, expected', [
-    (PATH + 'file1.txt', PATH + 'file2.json', 'stylish', None),
-    (PATH + 'file1.json', PATH + 'file1.txt', 'stylish', None)
-])
-def test_gendiff_wrong_extension(file_path1, file_path2, formatter, expected):
-    assert generate_diff(file_path1, file_path2, formatter) == expected
+    with open(expected) as f:
+        expected_out = f.read()
+    assert generate_diff(file_path1, file_path2, formatter) == expected_out
 
 
 def test_gendiff_wrong_formatter():
-    assert generate_diff(PATH + 'file1.json', PATH + 'file2.json', 'some') is None
+    assert generate_diff(f'{PATH}file1.json', f'{PATH}file2.json', 'some') is None
