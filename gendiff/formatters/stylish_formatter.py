866f36ef04fe93f7fc900f9c66c9ffdd24b5ec79
Dmitry <di.mesler.2010@yandex.ru>
2023-05-02 17:28:19 +0300

    Refactoring: delete str concat, add f-strings

--- gendiff/formatters/stylish_formatter.py
+++ gendiff/formatters/stylish_formatter.py
@@ -31,14 +31,14 @@ def calculate_value(val, deep):
     if isinstance(val, dict):
         if len(val) == 1:
             key, value = list(val.items())[0]
-            return '{\n' + f'{get_space(deep + 1)}{key}: ' \
-                            f'{calculate_value(value, deep + 1)}' \
-                + f'\n{get_space(deep)}' + '}'
+            return f'{"{"}\n{get_space(deep + 1)}{key}: ' \
+                   f'{calculate_value(value, deep + 1)}' \
+                   f'\n{get_space(deep)}{"}"}'
         list_ = []
         for key, values in val.items():
             list_.append(f'\n{get_space(deep + 1)}{key}: '
                          f'{calculate_value(values, deep + 1)}')
-        return '{' + ''.join(list_) + f'\n{get_space(deep)}' + '}'
+        return f'{"{"}{"".join(list_)}\n{get_space(deep)}{"}"}'
     return transform_value(val)
 
 
@@ -49,7 +49,7 @@ def build_stylish_tree(dict_, deep):
         if status == 'NESTED':
             result = '\n'.join(list(map(
                 lambda x: build_stylish_tree(x, deep + 1), dict_['value'])))
-            value = '{' + f'\n{result}' + f'\n{get_space(deep + 1)}' + '}'
+            value = f'{"{"}\n{result}\n{get_space(deep + 1)}{"}"}'
             return key + value
         value = calculate_value(dict_['value'], deep + 1)
         return key + value
